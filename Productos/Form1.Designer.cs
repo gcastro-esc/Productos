065b60f947d6033176eb461cd95422c1e3e3979d
gcastro-esc <jg.castro.e@gmail.com>
2024-12-10 11:05:49 -0600

    Proyecto Agregado

---
+++ Productos/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace Productos
+{
+    partial class frmVentas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmVentas));
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.picProducto = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDescripción = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbCategoria = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.numCantidad = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lvCompras = new System.Windows.Forms.ListView();
+            this.picLogo = new System.Windows.Forms.PictureBox();
+            this.lblSubTotal = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picProducto)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numCantidad)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.lblTitulo.Location = new System.Drawing.Point(207, 9);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(121, 19);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Super Market";
+            // 
+            // picProducto
+            // 
+            this.picProducto.Location = new System.Drawing.Point(448, 99);
+            this.picProducto.Name = "picProducto";
+            this.picProducto.Size = new System.Drawing.Size(147, 119);
+            this.picProducto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picProducto.TabIndex = 2;
+            this.picProducto.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Descripción";
+            // 
+            // txtDescripción
+            // 
+            this.txtDescripción.Location = new System.Drawing.Point(112, 122);
+            this.txtDescripción.Name = "txtDescripción";
+            this.txtDescripción.Size = new System.Drawing.Size(287, 20);
+            this.txtDescripción.TabIndex = 4;
+            this.txtDescripción.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 178);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Categoria";
+            // 
+            // cbCategoria
+            // 
+            this.cbCategoria.FormattingEnabled = true;
+            this.cbCategoria.Items.AddRange(new object[] {
+            "Alimentos Frescos",
+            "Alimentos Enlatados",
+            "Limpieza e Higiene",
+            "Vestido y Ropa",
+            "Articulos de Oficina"});
+            this.cbCategoria.Location = new System.Drawing.Point(112, 173);
+            this.cbCategoria.Name = "cbCategoria";
+            this.cbCategoria.Size = new System.Drawing.Size(287, 21);
+            this.cbCategoria.TabIndex = 6;
+            this.cbCategoria.SelectedIndexChanged += new System.EventHandler(this.cbCategoria_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(52, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Precio";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(112, 221);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(101, 20);
+            this.txtPrecio.TabIndex = 8;
+            this.txtPrecio.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtPrecio.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPrecio_KeyPress);
+            // 
+            // numCantidad
+            // 
+            this.numCantidad.Location = new System.Drawing.Point(112, 264);
+            this.numCantidad.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numCantidad.Name = "numCantidad";
+            this.numCantidad.Size = new System.Drawing.Size(101, 20);
+            this.numCantidad.TabIndex = 9;
+            this.numCantidad.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(32, 268);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Cantidad";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(15, 302);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(198, 41);
+            this.btnAgregar.TabIndex = 11;
+            this.btnAgregar.Text = "Agregar Producto";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(401, 302);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(194, 41);
+            this.btnEliminar.TabIndex = 12;
+            this.btnEliminar.Text = "Eliminar Producto";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(192, 359);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(165, 16);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "- Lista de Compras -";
+            // 
+            // lvCompras
+            // 
+            this.lvCompras.Location = new System.Drawing.Point(15, 378);
+            this.lvCompras.Name = "lvCompras";
+            this.lvCompras.Size = new System.Drawing.Size(580, 97);
+            this.lvCompras.TabIndex = 14;
+            this.lvCompras.UseCompatibleStateImageBehavior = false;
+            // 
+            // picLogo
+            // 
+            this.picLogo.Image = global::Productos.Properties.Resources.screencart_106583;
+            this.picLogo.Location = new System.Drawing.Point(206, 34);
+            this.picLogo.Name = "picLogo";
+            this.picLogo.Size = new System.Drawing.Size(111, 82);
+            this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picLogo.TabIndex = 15;
+            this.picLogo.TabStop = false;
+            // 
+            // lblSubTotal
+            // 
+            this.lblSubTotal.AutoSize = true;
+            this.lblSubTotal.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSubTotal.Location = new System.Drawing.Point(417, 488);
+            this.lblSubTotal.Name = "lblSubTotal";
+            this.lblSubTotal.Size = new System.Drawing.Size(75, 16);
+            this.lblSubTotal.TabIndex = 16;
+            this.lblSubTotal.Text = "SubTotal";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 488);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(213, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Desarrollado por Camila Juárez Medina 3BP";
+            // 
+            // frmVentas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.ClientSize = new System.Drawing.Size(607, 513);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblSubTotal);
+            this.Controls.Add(this.picLogo);
+            this.Controls.Add(this.lvCompras);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numCantidad);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbCategoria);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDescripción);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picProducto);
+            this.Controls.Add(this.lblTitulo);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmVentas";
+            this.Text = "Ventas - Super Market";
+            this.Load += new System.EventHandler(this.frmVentas_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picProducto)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numCantidad)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.PictureBox picProducto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDescripción;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbCategoria;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.NumericUpDown numCantidad;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListView lvCompras;
+        private System.Windows.Forms.PictureBox picLogo;
+        private System.Windows.Forms.Label lblSubTotal;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Productos/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Productos
+{
+    public partial class frmVentas : Form
+    {
+        double subTotal = 0, Pago = 0;
+        public frmVentas()
+        {
+            InitializeComponent();
+        }
+
+        private void txtPrecio_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 8)
+            {
+                e.Handled = true;
+            }
+        }
+        private void cbCategoria_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int indice = cbCategoria.SelectedIndex;
+            switch (indice)
+            {
+                case 0:
+                    picProducto.Image = Productos.Properties.Resources.fresco;
+                    break;
+                case 1:
+                    picProducto.Image = Productos.Properties.Resources.Enlatados;
+                    break;
+                case 2:
+                    picProducto.Image = Productos.Properties.Resources.Higiene;
+                    break;
+                case 3:
+                    picProducto.Image = Productos.Properties.Resources.Ropa;
+                    break;
+                case 4:
+                    picProducto.Image = Productos.Properties.Resources.Oficina;
+                    break;
+            }
+        }
+
+        private void frmVentas_Load(object sender, EventArgs e)
+        {
+            lvCompras.View = View.Details;
+            lvCompras.Columns.Add("Descripción", 150, HorizontalAlignment.Center);
+            lvCompras.Columns.Add("Categoría", 150, HorizontalAlignment.Center);
+            lvCompras.Columns.Add("Precio", 100, HorizontalAlignment.Center);
+            lvCompras.Columns.Add("Cantidad", 100, HorizontalAlignment.Center);
+            lvCompras.Columns.Add("A pagar", 100, HorizontalAlignment.Center);
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if ((string.IsNullOrWhiteSpace(txtDescripción.Text) || cbCategoria.SelectedIndex < 0 || string.IsNullOrWhiteSpace(txtPrecio.Text)))
+            {
+                MessageBox.Show("No se han ingresado todos los detalles del producto, intenta nuevamente", "Datos incompletos");
+            }
+            else
+            {
+                string producto, categoria;
+                double precio;
+                int cantidad;
+                producto = txtDescripción.Text;
+                categoria = cbCategoria.SelectedItem.ToString();
+                precio = Convert.ToDouble(txtPrecio.Text);
+                cantidad = Convert.ToInt32(numCantidad.Value);
+
+                Pago = precio * cantidad;
+
+                ListViewItem elemento = new ListViewItem(producto);
+                elemento.SubItems.Add(categoria);
+                elemento.SubItems.Add(precio.ToString());
+                elemento.SubItems.Add(cantidad.ToString());
+                elemento.SubItems.Add(Pago.ToString());
+                lvCompras.Items.Add(elemento);
+
+                subTotal = subTotal + Pago;
+                lblSubTotal.Text = "SubTotal - $ " + Math.Round(subTotal, 2);
+
+                txtDescripción.Clear();
+                cbCategoria.SelectedIndex = -1;
+                txtPrecio.Clear();
+                numCantidad.Value = 1;
+                picProducto.Image = null;
+                txtDescripción.Focus();
+            }     
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (lvCompras.SelectedItems.Count > 0)
+            {
+                ListViewItem elemento = lvCompras.SelectedItems[0];
+                Pago = Convert.ToDouble(elemento.SubItems[4].Text);
+                lvCompras.Items.Remove(elemento);
+                subTotal = subTotal - Pago;
+                lblSubTotal.Text = "SubTotal - $ " + Math.Round(subTotal, 2);
+                MessageBox.Show("Producto eliminado correctamente");
+            }
+            else
+            {
+                MessageBox.Show("Por favor, selecciona un producto para eliminar", "Sin selección");
+            }
+        }
+    }
+}
